d5699e75fdbe0678c59e3235b7dd81609177857a
jesna77 <jesna2772@gmail.com>
2017-01-21 10:34:30 +0530

    functionality 1 is completed

---
+++ Jesna.java
@@ -0,0 +1,13 @@
+package com.iss.core.abstracts;
+
+public class Jesna {
+
+
+	public static void main(String[] args) {
+		String name="ABC";
+		for (int i = 0; i <= 10; i++) {
+			System.out.println("name"+name);
+		}
+	}
+
+}
