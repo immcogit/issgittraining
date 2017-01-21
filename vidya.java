34e81359e166ce3e32ac669951aa547e99a3f9fd
training <training>
2017-01-21 10:28:24 +0530

    Functionality is added

---
+++ vidya.java
@@ -0,0 +1,10 @@
+public class Vidya{
+    private String name;
+    public void set(String name){
+         this.name = name;
+    }
+    public void get(){
+         system.out.println(name);
+    }
+
+}
